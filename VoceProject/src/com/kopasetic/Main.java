a70d7b6fcdcafe274ea2fd2eb220bae0d2d77538
Mark Haney <blah@blah.com>
2016-09-20 23:26:40 -0700

    in and out working, added weather but issue with api

--- VoceProject/src/com/kopasetic/Main.java
+++ VoceProject/src/com/kopasetic/Main.java
@@ -1,23 +1,28 @@
 package com.kopasetic;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+//import org.apache.log4j.Logger;
+
+// api doco:  http://voce.sourceforge.net/api/java/classvoce_1_1_speech_interface.html
+// http://cmusphinx.sourceforge.net/doc/sphinx4/edu/cmu/sphinx/jsgf/JSGFGrammar.html
+
 public class Main {
     public static void main(String[] argv)
     {
         System.out.println("starting main...");
-        voce.SpeechInterface.init("./lib", false, true,
-                "./grammar", "digits");
+        voce.SpeechInterface.init("./lib", true, true,
+                "./grammar", "main");
 
-        System.out.println("This is a speech recognition test. "
-                + "Speak digits from 0-9 into the microphone. "
-                + "Speak 'quit' to quit.");
+        System.out.println("Minion engine initialized.");
+        voce.SpeechInterface.synthesize("I am minion.");
 
         boolean quit = false;
         while (!quit)
         {
-
-            // Normally, applications would do application-specific things
-            // here.
-            // For this sample, we'll just sleep for a little bit.
             try
             {
                 Thread.sleep(300);
@@ -34,6 +39,12 @@ public class Main {
                 if (-1 != s.indexOf("quit"))
                 {
                     quit = true;
+                } else if (-1 != s.indexOf("hello world")) {
+                    System.out.println("hello back at you.");
+                    voce.SpeechInterface.synthesize("well hello to you too beautiful!");
+                } else if (-1 != s.indexOf("weather")) {
+                    System.out.println(ServiceWeather.get());
+                    //voce.SpeechInterface.synthesize("well hello to you too beautiful!");
                 }
 
                 System.out.println("You said: " + s);
@@ -42,7 +53,10 @@ public class Main {
         }
 
         voce.SpeechInterface.destroy();
+        voce.SpeechInterface.stopSynthesizing();
+        voce.SpeechInterface.destroy();
         System.exit(0);
 
     }
+
 }
